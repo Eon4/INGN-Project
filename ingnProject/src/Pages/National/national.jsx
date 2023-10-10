cf5f07d06667401c5cd2d23fda9f1c2e9a758f3a
Cecilia <109989926+Eon4@users.noreply.github.com>
2023-10-10 14:17:03 +0200

    update

--- ingnProject/src/Pages/National/national.jsx
+++ ingnProject/src/Pages/National/national.jsx
@@ -1,23 +1,48 @@
 
-import React from 'react';
 import { useQuery } from '@tanstack/react-query';
-import { Link, useParams } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import { getNational } from '../../Queries/National';
+import { request } from "graphql-request";
 // import style from '../../Pages/HomePage/homePage.module.scss';
 
 export const National = () => {
-  
-  return (
-    <div>
-      <h2>National page is right here</h2>
+
+  const { data, isLoading, error } = useQuery({
+    queryKey: ['getNational'],
+    queryFn: async () =>
+      request(import.meta.env.VITE_PUBLIC_URL_ID, getNational),
+  });
+  console.log(data)
+
+
+  if (isLoading) {
+    return <p>Loading...</p>;
+  }
+
+  if (error) {
+    return <p>Error: {error.message}</p>;
+  }
+
+  if (!data) {
+    return <p>Data is undefined</p>; 
+  }
   
 
-      {/* <h3>
-        <Link to="/blogPage" className={style.LatestArticlesLink}>
-          Latest Blogs
-        </Link>
-      </h3> */}
-    </div>
+  return (
+    <>
+      {data.artikler.map((item, index) => {
+        return (
+          <article key={index}>
+            <h2>{item.articleTitle}</h2>
+            <p>
+              D. {item.date} - af {item.author}
+            </p>
+            <img src={item.image.url} alt={item.descriptionOfImage} />
+            <Link to={`/${item.id}`}>Læs mere</Link>
+          </article>
+        );
+      })}
+    </>
   );
 };
 
--- ingnProject/src/Queries/National.js
+++ ingnProject/src/Queries/National.js
@@ -1,18 +1,18 @@
 
 export const getNational = `
-query International {
-    query MyArticles {
-        artikler(where: {category_contains_all: "National"}) {
-          articleTitle
-          date
-          author
-          image {
-            url
-          }
-          articleContent {
-            text
-          }
-        }
+query MyArticles {
+    artikler(where: {category_contains_all: "National"}) {
+        id
+      articleTitle
+      date
+      author
+      image {
+        url
       }
-}
+      articleContent {
+        text
+      }
+    }
+  }
+  
 `;
