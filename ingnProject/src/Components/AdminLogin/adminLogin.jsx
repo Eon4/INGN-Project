62cd7e418a337b13c0be0afd117d4be9d5d4f66f
Cecilia <109989926+Eon4@users.noreply.github.com>
2023-10-12 15:23:08 +0200

    update for tomorrow

---
+++ ingnProject/src/Components/AdminLogin/adminLogin.jsx
@@ -0,0 +1,12 @@
+
+import style from '../../Components/AdminLogin/adminLogin';
+
+const AdminLogin = () => {
+  return (
+    <div className={style.AdminContainer}>
+      <h2>Homepage is here</h2>
+    </div>
+  );
+};
+
+export default AdminLogin;
