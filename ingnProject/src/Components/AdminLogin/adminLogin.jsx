375d13a816e826d52be92821a37cf82e6cee095d
Cecilia <109989926+Eon4@users.noreply.github.com>
2023-10-12 15:23:28 +0200

    update

--- ingnProject/src/Components/AdminLogin/adminLogin.jsx
+++ ingnProject/src/Components/AdminLogin/adminLogin.jsx
@@ -4,7 +4,7 @@ import style from '../../Components/AdminLogin/adminLogin';
 const AdminLogin = () => {
   return (
     <div className={style.AdminContainer}>
-      <h2>Homepage is here</h2>
+      <h2>AdminPage is here</h2>
     </div>
   );
 };
